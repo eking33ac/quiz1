bcb9e207d10db466e15ad138494a09369347d37d
Ezri <eking33@amarillocollege.com>
2025-01-15 13:49:49 -0600

    first commit

---
+++ Quiz1/Quiz1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is Quiz 1.");       // Display a line in the console
+
+            Console.ReadLine();     // Computer reads a line user writes in the console
+        }
+    }
+}
